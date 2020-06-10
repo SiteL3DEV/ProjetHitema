93ecc9fd55079c8d4d4b6f9d1b7daec94f88726f
yann <yann.haocas@gmail.com>
2020-06-10 15:42:31 +0200

    afficher les annonces

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonce;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,13 +18,27 @@ class AccueilController extends AbstractController
   public function index(Environment $twig)
   {
     $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-    /** @var \App\Entity\User $user */
+    /** @var \App\Entity\Utilisateurs $user */
     $user = $this->getUser();
     $btnLogInOut = 'Se connecter';
+    $name = '';
+
+    $em = $this->getDoctrine()->getManager();
+    $listAnnonces = $em->getRepository(Annonce::class)->findAll();
+    if (!$listAnnonces) {
+      throw $this->createNotFoundException(
+          'Aucune annonce touvée'
+      );
+    }
     if($user){
+      $name = $user->getNom().' '.$user->getPrenom();
       $btnLogInOut = 'Se déconnecter';
     }
-    $content = $twig->render('index.html.twig', ['loginout' => $btnLogInOut]);
+    $content = $twig->render('index.html.twig', [
+                                                  'loginout' => $btnLogInOut,
+                                                  'name' => $name,
+                                                  'annonces' => $listAnnonces
+                                                ]);
 
     return new Response($content);
   }
