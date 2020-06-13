36b95749d2690e467e20e164d2a01335b6553703
yann <yann.haocas@gmail.com>
2020-06-13 10:58:08 +0200

    nettoyage et restructuration

--- assets/js/accueil.js
+++ assets/js/accueil.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
           let html = '';
           $.each(tabAnnonces, function( index, value ) {
             html+=`<div class="col-md-4">
-            <div style="height:auto !important;" class="card">
-              <img style="width:200px; height:200px;" src="/ProjetHitema/public/build/img/annonces/` + value['image'] + `" class="card-img-top ml-auto mr-auto" alt="...">
-              <div class="card-body">
-                <p class="card-text">` + value['description'] + `</p>
-                <center><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Accéder au produit</button></center>
+              <div style="height:auto !important;" class="card">
+                <img style="width:200px; height:200px;" src="/ProjetHitema/public/build/img/annonces/` + value['image'] + `" class="card-img-top ml-auto mr-auto" alt="...">
+                <div class="card-body">
+                  <p class="card-text">` + value['description'] + `</p>
+                  <center><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Accéder au produit</button></center>
+                </div>
               </div>
-            </div>
-          </div>`
+            </div>`
           });
           $('.contentannonces').html(html);          
         });
